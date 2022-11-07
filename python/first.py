8b20a4c2925dac9d5c200de78a28f2c8bc0e789f
mohammad jamshidian <jamshidian9898@gmail.com>
2022-11-07 07:09:58 +0000

    change print format

--- python/first.py
+++ python/first.py
@@ -2,25 +2,25 @@ a = 10
 b = 20.5
 
 c = a + b
-print('a + b = %d'%c)
+print('a + b = ' ,c)
 
 c = a - b
-print('a - b = %d'%c)
+print('a - b = ', c)
 
 c = a * b
-print('a * b = %d'%c)
+print('a * b = ', c)
 
 c = a / b
-print('a * b = %d'%c)
+print('a / b = ', c)
 
 c = a // b
-print('a * b = %d'%c)
+print('a // b = ', c)
 
 c = a ** b
-print('a * b = %d'%c)
+print('a ** b = ', c)
 
 c = a ^ int(b)
-print('a * b = %d'%c)
+print('a ^ b = ', c)
 
 c = a % b
-print('a * b = %d'%c)
+print('a % b = ', c)
