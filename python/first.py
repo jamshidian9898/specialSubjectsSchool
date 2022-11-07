db550bfdb7b264e8387d536daf6d93b7663b32cd
mohammad jamshidian <jamshidian9898@gmail.com>
2022-11-07 07:05:42 +0000

    add more opration

--- python/first.py
+++ python/first.py
@@ -1,5 +1,26 @@
 a = 10
-b = 20
+b = 20.5
+
 c = a + b
-print("C: ")
-print(c)
+print('a + b = %d'%c)
+
+c = a - b
+print('a - b = %d'%c)
+
+c = a * b
+print('a * b = %d'%c)
+
+c = a / b
+print('a * b = %d'%c)
+
+c = a // b
+print('a * b = %d'%c)
+
+c = a ** b
+print('a * b = %d'%c)
+
+c = a ^ int(b)
+print('a * b = %d'%c)
+
+c = a % b
+print('a * b = %d'%c)
